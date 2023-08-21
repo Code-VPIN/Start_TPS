608a63dd0dcdc2ea926042677493af3fb143de40
Vipin Saroj <vipins@tpstech.in>
2023-08-21 10:40:27 +0530

    status update minor content change changes

--- start_tps/js/main.js
+++ start_tps/js/main.js
@@ -47,3 +47,15 @@ contactForm.addEventListener('submit', sendEmail)
 
 // sidebar tas recube js
 
+const mainItems = document.querySelectorAll(
+    '.main-item'
+  );
+  
+  mainItems.forEach((mainItem) => {
+    mainItem.addEventListener('click', () => {
+      mainItem.classList.toggle(
+        'main-item--open'
+      );
+    })
+  });
+
