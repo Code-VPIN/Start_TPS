bff8efabbac9afec829dccaa636053e82122bf99
Vipin Saroj <vipins@tpstech.in>
2023-08-22 09:54:12 +0530

    Pricing Module Added links color changes

--- start_tps/js/main.js
+++ start_tps/js/main.js
@@ -1,17 +1,17 @@
 // scroll animation js start
 
 const sr = ScrollReveal({
-    origin:'top',
-    distance:'60px',
-    duration: 2500,
-    delay:400,
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
 })
 
-sr.reveal(`.home__perfil, .about__image, .contact__mail`, {origin:'origin'})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`, { origin: 'origin' })
 sr.reveal(`.home__name, .home__info, 
-            .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, {origin:'left'})
+            .about__container, .section__title-1, .about__info, .contact__social, .contact__data`, { origin: 'left' })
 
-sr.reveal(`.services__card, .projects__cards`, {origin:'100'})
+sr.reveal(`.services__card, .projects__cards`, { origin: '100' })
 
 // scroll animation js end
 
@@ -20,23 +20,23 @@ sr.reveal(`.services__card, .projects__cards`, {origin:'100'})
 // email javascript
 
 const contactForm = document.getElementById('contact-form')
-    contactMessage = document.getElementById('contact-message')
+contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    emailjs.sendForm('service_8t4z5kp','template_xfg5w7c','#contact-form','rwEWS9_LLtDo4HN0N')
-    .then(()=>{
-        contactMessage.textContent ='Message Sent Successfully.'
+  emailjs.sendForm('service_8t4z5kp', 'template_xfg5w7c', '#contact-form', 'rwEWS9_LLtDo4HN0N')
+    .then(() => {
+      contactMessage.textContent = 'Message Sent Successfully.'
 
-        setTimeout(()=>{
-            contactMessage.textContent = ''
-        }, 5000 )
+      setTimeout(() => {
+        contactMessage.textContent = ''
+      }, 5000)
 
-        contactForm.reset()
+      contactForm.reset()
 
-    }), () =>{
-            contactMessage.textContent='Message Not Sent (Service Error)'
+    }), () => {
+      contactMessage.textContent = 'Message Not Sent (Service Error)'
     }
 
 }
@@ -48,14 +48,22 @@ contactForm.addEventListener('submit', sendEmail)
 // sidebar tas recube js
 
 const mainItems = document.querySelectorAll(
-    '.main-item'
-  );
-  
-  mainItems.forEach((mainItem) => {
-    mainItem.addEventListener('click', () => {
-      mainItem.classList.toggle(
-        'main-item--open'
-      );
-    })
-  });
+  '.main-item'
+);
+
+mainItems.forEach((mainItem) => {
+  mainItem.addEventListener('click', () => {
+    mainItem.classList.toggle(
+      'main-item--open'
+    );
+  })
+});
+
+
+// pricing module animation js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const rows = document.querySelectorAll(".animate-row");
+//   rows.forEach(row => row.classList.add("animate-row"));
+// });
 
